40acb85bf758764c347198812b5890dc3a7e3953
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-06-15 20:45:04 +0530

    base: Introduce Smart 5G service [1/2]
    Current functionality:
    - Disable 5G in battery saver mode
    - Disable 5G when not on mobile data (such as wifi)
    - Disable 5G on non-active data sim
    
    Not smart enough yet, but we'll get there.
    
    Change-Id: Iac71c1de51429e2287162bb9e45ff4aba3bd0ac9

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -1735,6 +1735,10 @@ public final class SystemServer implements Dumpable {
             mSystemServiceManager.startService(LogcatManagerService.class);
             t.traceEnd();
 
+            t.traceBegin("StartSmart5gService");
+            mSystemServiceManager.startService(Smart5gService.class);
+            t.traceEnd();
+
             if (AppFunctionManagerConfiguration.isSupported(context)) {
                 t.traceBegin("StartAppFunctionManager");
                 mSystemServiceManager.startService(AppFunctionManagerService.class);
